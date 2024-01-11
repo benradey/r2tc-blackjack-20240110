94d4ac69f7e46c550c340d30015197852aae9e6f
Ted M. Young <tedyoung@gmail.com>
2024-01-10 16:32:59 -0800

    LAB #4: Changed Deck to use Suit enum instead of String symbols

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -8,12 +8,6 @@ public class Card {
     private final Suit suit;
     private final String rank;
 
-    @Deprecated // this should go away
-    public Card(String symbol, String rank) {
-        this.suit = Suit.from(symbol);
-        this.rank = rank;
-    }
-
     public Card(Suit suit, String rank) {
         this.suit = suit;
         this.rank = rank;
